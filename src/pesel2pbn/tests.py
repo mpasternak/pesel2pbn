bde87ba5d86358b60d073a65420e6154d3d83ec1
Michał Pasternak @ THINKPAD (win32) <michal.dtz@gmail.com>
2015-07-11 19:55:18 +0200

    Formatowanie

--- src/pesel2pbn/tests.py
+++ src/pesel2pbn/tests.py
@@ -7,7 +7,8 @@ from PyQt5.QtWidgets import QMessageBox, QApplication
 import pytest
 from pesel2pbn.main import Pesel2PBNWindow
 
-WYGENEROWANY_PESEL = '00891702980'
+WYGENEROWANY_PESEL = '00891702980'  # http://www.bogus.ovh.org/generatory/all.html
+
 
 @pytest.fixture
 def p2p_window(qtbot):
@@ -16,6 +17,7 @@ def p2p_window(qtbot):
     qtbot.addWidget(window)
     return window
 
+
 def test_pesel2pbn_pastePESEL(qtbot, p2p_window):
     with mock.patch('PyQt5.QtWidgets.QApplication.clipboard') as cb:
         clipboard = cb.return_value
@@ -23,6 +25,7 @@ def test_pesel2pbn_pastePESEL(qtbot, p2p_window):
 
         qtbot.mouseClick(p2p_window.ui.pastePESEL, QtCore.Qt.LeftButton)
         assert p2p_window.ui.numeryPESEL.toPlainText() == '123'
+
 
 def test_pesel2pbn_copyPBN(qtbot, p2p_window):
     with mock.patch('PyQt5.QtWidgets.QApplication.clipboard') as cb:
@@ -33,15 +36,18 @@ def test_pesel2pbn_copyPBN(qtbot, p2p_window):
         qtbot.mouseClick(p2p_window.ui.copyPBN, QtCore.Qt.LeftButton)
         assert clipboard.setText.called_with("123", mode=clipboard.Clipboard)
 
+
 def test_about_box(qtbot, p2p_window):
     with mock.patch.object(QMessageBox, 'about', new=Mock) as about:
         qtbot.mouseClick(p2p_window.ui.oProgramie, QtCore.Qt.LeftButton)
         assert about.called
 
+
 def test_wykonaj_brak_PESELi(qtbot, p2p_window):
     with mock.patch.object(QMessageBox, 'critical', new=Mock) as critical:
         qtbot.mouseClick(p2p_window.ui.wykonajButton, QtCore.Qt.LeftButton)
         assert critical.called
+
 
 def test_wykonaj_zle_PESELe(qtbot, p2p_window):
     p2p_window.ui.numeryPESEL.setPlainText('123')
@@ -50,12 +56,14 @@ def test_wykonaj_zle_PESELe(qtbot, p2p_window):
         qtbot.mouseClick(p2p_window.ui.wykonajButton, QtCore.Qt.LeftButton)
         assert critical.called
 
+
 def test_wykonaj_dobre_PESELe_bez_tokena(qtbot, p2p_window):
     p2p_window.ui.numeryPESEL.setPlainText(WYGENEROWANY_PESEL)
 
     with mock.patch.object(QMessageBox, 'critical', new=Mock) as critical:
         qtbot.mouseClick(p2p_window.ui.wykonajButton, QtCore.Qt.LeftButton)
         assert critical.called
+
 
 def test_wykonaj_dobre_PESELe_bez_tokena(qtbot, p2p_window):
     p2p_window.ui.numeryPESEL.setPlainText(WYGENEROWANY_PESEL)
